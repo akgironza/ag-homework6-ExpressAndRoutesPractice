04c258e207e762b8c0925d21d4b9a3044ee5e995
Arielle Gironza <ariellegironza@Arielles-MacBook-Air.local>
2023-06-12 23:21:58 -0500

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-// initial setup
+// INITIAL SETUP
 const express = require("express")
 const app = express()
 
 
-// routes
+// ROUTES
+
+// greeting
 app.get("/greeting/:name", (req, res) => {
     // grab the name from the params
     const name = req.params.name
@@ -11,7 +13,15 @@ app.get("/greeting/:name", (req, res) => {
     res.send(`Hey, ${name}! How's it going?`)
 })
 
-// listener
+// tip calculator
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+    const total = req.params.total
+    const tipPercentage = `0.${req.params.tipPercentage}`
+    const totalTip = total * tipPercentage
+    res.send(`Tip for bill amount: $${totalTip}`)
+})
+
+// LISTENER
 app.listen(3500, () => {
     console.log("Testing, testing...")
 })
